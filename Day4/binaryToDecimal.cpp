fbd49cb5f2d6f8fa9241d11b00400b8e42fe031c
Fahimul Islam Sifat <86623478+sifat-fahimul@users.noreply.github.com>
2022-12-22 11:18:11 +0600

    binary to decimal in c++

---
+++ Day4/binaryToDecimal.cpp
@@ -0,0 +1,28 @@
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int i = 0;
+    int ans = 0;
+
+    while (n != 0)
+    {
+        // int bit=n&1;
+        int digit = n % 10;
+        if (digit == 1)
+        {
+            ans = ans + pow(2, i);
+        }
+        // n=n>>1;
+        n = n / 10;
+        i++;
+    }
+    std::cout << ans << std::endl;
+    return 0;
+}
