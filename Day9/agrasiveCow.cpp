3637008cdaf7064ae8cbc321159620869edb240e
Fahimul Islam Sifat <86623478+sifat-fahimul@users.noreply.github.com>
2023-01-01 18:08:35 +0600

    agrasive cow solve the problem

---
+++ Day9/agrasiveCow.cpp
@@ -0,0 +1,50 @@
+bool isPossible(vector<int> &stalls, int k, int mid, int n)
+{
+
+    int cowCount = 1;
+    int lastPos = stalls[0];
+
+    for (int i = 0; i < n; i++)
+    {
+
+        if (stalls[i] - lastPos >= mid)
+        {
+            cowCount++;
+            if (cowCount == k)
+            {
+                return true;
+            }
+            lastPos = stalls[i];
+        }
+    }
+    return false;
+}
+
+int aggressiveCows(vector<int> &stalls, int k)
+{
+    sort(stalls.begin(), stalls.end());
+    int s = 0;
+    int n = stalls.size();
+    int maxi = -1;
+    for (int i = 0; i < n; i++)
+    {
+        maxi = max(maxi, stalls[i]);
+    }
+    int e = maxi;
+    int mid = s + (e - s) / 2;
+    int ans = -1;
+    while (s <= e)
+    {
+        if (isPossible(stalls, k, mid, n))
+        {
+            ans = mid;
+            s = mid + 1;
+        }
+        else
+        {
+            e = mid - 1;
+        }
+        mid = s + (e - s) / 2;
+    }
+    return ans;
+}
