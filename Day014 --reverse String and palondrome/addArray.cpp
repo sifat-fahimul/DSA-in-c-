bbc3bf62d669649c603abc555ea5bd2ddc5a54ae
Fahimul Islam Sifat <86623478+sifat-fahimul@users.noreply.github.com>
2023-01-09 13:31:54 +0600

    reverse array and check palindrome

---
+++ Day014 --reverse String and palondrome/addArray.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char name[20];
+
+    cout << "Enter Your Name: " << endl;
+    cin >> name;
+
+    cout << "Your Name is : ";
+    cout << name << endl;
+
+    return 0;
+}
