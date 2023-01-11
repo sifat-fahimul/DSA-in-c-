85bb7efcb299b89aa27b4878199427563b2c6df2
Fahimul Islam Sifat <86623478+sifat-fahimul@users.noreply.github.com>
2023-01-11 13:24:08 +0600

    2D array problem solving

---
+++ Day016 --2D array/Search2DMatrix_II.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int row=matrix.size();
+        int col=matrix[0].size();
+
+        int rowIndex=0;
+        int colIndex=col-1;
+
+        while(rowIndex<row && colIndex>=0){
+            int el=matrix[rowIndex][colIndex];
+
+            if(el==target){
+                return 1;
+            }
+            if(el<target){
+                rowIndex++;
+            }else{
+                colIndex--;
+            }
+        }
+        return 0;        
+    }
+};
---
+++ Day016 --2D array/rotate90deg.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+class Solution
+{
+public:
+    void rotate(vector<vector<int>> &matrix)
+    {
+        int row = matrix.size();
+        for (int i = 0; i < row; i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+        for (int j = 0; j < row; j++)
+        {
+            reverse(matrix[j].begin(), matrix[j].end());
+        }
+    }
+};
